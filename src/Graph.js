e6dae0bd6be714da942b8c666121a7e7da62566e
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-12 01:40:33 +0300

    :tada: add graph component

---
+++ src/Graph.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import { Line } from 'react-chartjs-2';
+
+function Graph() {
+  const [data, setData] = useState({})
+
+  const API_CHART_URL = 'https://disease.sh/v3/covid-19/historical/all?lastdays=120';
+
+  useEffect(() => {
+    fetch(API_CHART_URL)
+    .then((response) => response.json())
+    .then((data) => {
+
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1> I am a graph </h1>
+      <Line data options/>
+    </div>
+  )
+}
+
+export default Graph
