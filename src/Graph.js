803566750b4565217de9b33a69c1a01a91d55a1e
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-12 02:26:03 +0300

    :zap: build chart component

--- src/Graph.js
+++ src/Graph.js
@@ -1,23 +1,84 @@
 import React, {useState, useEffect} from 'react';
+import { buildChartData } from './utils';
 import { Line } from 'react-chartjs-2';
+import numeral from 'numeral';
 
-function Graph() {
-  const [data, setData] = useState({})
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: 'index',
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format('+0,0');
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: 'time',
+        time: {
+          format: 'MM/DD/YY',
+          tootipFormat: 'll',
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format('0a');
+          },
+        },
+      },
+    ],
+  },
+}
+
+function Graph({ caseType = 'cases' }) {
+  const [data, setData] = useState({});
 
   const API_CHART_URL = 'https://disease.sh/v3/covid-19/historical/all?lastdays=120';
 
   useEffect(() => {
-    fetch(API_CHART_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-  }, [])
+    const fetchData = async () => {
+      await fetch(API_CHART_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        const chartData = buildChartData(data);
+        setData(chartData);
+      })
+    }
+    fetchData();
+  }, [caseType]);
 
   return (
     <div>
-      <h1> I am a graph </h1>
-      {/* <Line data options/> */}
+      <h1> New Cases </h1>
+      {data?.length > 0 && (
+        <Line 
+          options={options}
+          data={{
+            datasets: [{
+              backgroundColor: 'rgba(204, 16, 52, .5)',
+              borderColor: "#CC1034",
+              data: data
+            }]
+          }}
+        />
+      )}
     </div>
   )
 }
