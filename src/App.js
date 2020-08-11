dae80bc23581469665b49e68f51178ec84a7f006
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-11 22:20:26 +0300

    :zap: created header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import { MenuItem, FormControl, Select }  from '@material-ui/core';
 
 function App() {
+
+  
+
   return (
     <div className="app">
-      <h1> Hello </h1>
-
+      <div className='app__header'>        
+      <h1> COVID-19 TRACKER </h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value='abc'>
+          
+        </Select>
+      </FormControl>
+      </div>
     </div>
   );
 }
