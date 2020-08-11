d635dfbc6cdebb65095dc45ad4da361afdff0c3f
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-12 02:25:31 +0300

    :zap: added build chart data

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,19 @@ export const sortData = (data) => {
   const sortedData = [...data];
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 }
+
+export const buildChartData = (data, casesType='cases') => {
+  const chartData = [];
+  let lastDataPoint;
+  for(let date in data.cases) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      }
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+} 
