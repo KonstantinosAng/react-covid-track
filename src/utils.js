4426ec1a90be851e74e6d3871c7e1e5ce9249bf6
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-12 16:47:52 +0300

    :zap: added number format func

--- src/utils.js
+++ src/utils.js
@@ -68,3 +68,6 @@ export const showDataOnMap = (data, casesType='cases') => (
     </Circle>
   ))
 );
+
+export const prettyPrintStat = (stat) => 
+  stat ? `+${numeral(stat).format('0.0a')}` : "+0";
