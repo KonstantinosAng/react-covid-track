cd1580cb1de295289ae1ecc94ce5490860e1bbf5
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-12 03:18:59 +0300

    added circles on map

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './Map.css';
 import {Map as LeafletMap, TileLayer } from 'react-leaflet';
+import { showDataOnMap } from './utils';
 
 const URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-function Map({ center, zoom }) {
+function Map({ countries, casesType, center, zoom }) {
   return (
     <div className='map'>
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer url={URL} attribution='&copy; <a href="http://osm.org/copyright" />">OpenStreetMap</a> contributors'/>
+        {/* Loop through and draw circles */}
+        {showDataOnMap(countries, casesType)}
       </LeafletMap>
     </div>
   )
